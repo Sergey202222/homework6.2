2cba74911e42b9c8cb224dc4d586cb99b98c1b29
Sergey <57405954+Sergey202222@users.noreply.github.com>
2025-07-28 20:16:21 +0300

    Add files via upload

--- homework1/Script.sql
+++ homework1/Script.sql
@@ -1,33 +1,49 @@
-create table if not exists genre
+ CREATE TABLE IF NOT EXISTS Genre
 (
-  genre_name varchar(50) not null,
-  executor varchar(50) not null
+  genre_name VARCHAR(50) NOT NULL,
 );
 
-create table if not exists executor
+CREATE TABLE IF NOT EXISTS Executor
 (
-  executor_name varchar(50) not null,
-  genre varchar(50) not null,
-  albom varchar(50) not null
+  executor_name VARCHAR(50) NOT NULL,
+  
 );
 
-create table if not exists albom
+CREATE TABLE IF NOT EXISTS Albom
 (
-  albom_name varchar(50) not null,
-  year integer not null,
-  treck varchar(50) not null
+  albom_name VARCHAR(50) NOT NULL,
+  year INTEGER NOT NULL,
 );
 
-create table if not exists treck
+CREATE TABLE IF NOT EXISTS Treck
 (
-  treck_name varchar(50) not null,
-  time allballs not null,
-  collection varchar(50) not null
+  treck_name VARCHAR(50) NOT
+  time ALLBALLS NOT NULL,
 );
 
-create table if not exists collection
+CREATE TABLE IF NOT EXISTS Collection
 (
-  treck_name varchar(50) not null,
-  year integer not null,
-  treck varchar(50) not null
+  collection_name VARCHAR(50) NOT  NULL,
+  year INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Genre_executor
+(
+  genre_name VARCHAR(50) REFERENCES Genre(genre_name),
+  executor_name VARCHAR(50) REFERENCES Executor(executor_name),
+  CONSTRAINT GenreExecutor PRIMARY KEY (genre_name, executor_name)
+);
+
+CREATE TABLE IF NOT EXISTS Executor_albom
+(
+  executor_name VARCHAR(50) REFERENCES Executor(executor_name),
+  albom_name VARCHAR(50) REFERENCES Albom(albom_name),
+  CONSTRAINT genre_executor PRIMARY KEY (executor_name, albom_name)
+);
+
+CREATE TABLE IF NOT EXISTS Treck_collection
+(
+  treck_name VARCHAR(50) REFERENCES Treck (treck_name),
+  collection_name VARCHAR(50) REFERENCES Collection(collection_name),
+  CONSTRAINT genre_executor PRIMARY KEY (treck_name, collection_name)
 );
